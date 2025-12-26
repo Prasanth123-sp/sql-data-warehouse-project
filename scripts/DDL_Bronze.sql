67e4816f34e0e268da25de8fe37af8a0cff84679
Prasanth123-sp <prasanthsivakumar2002@gmail.com>
2025-12-26 20:06:07 +0530

    Update DDL_Bronze.sql

--- scripts/DDL_Bronze.sql
+++ scripts/DDL_Bronze.sql
@@ -22,8 +22,8 @@ create table bronze.crm_prd_info(
 	prd_nm varchar(50),
 	prd_cost int,
 	prd_line varchar(50),
-	prd_start_dt date,
-	prd_end_dt date
+	prd_start_dt DATETIME,
+	prd_end_dt DATETIME
 );
 go
 
@@ -35,9 +35,9 @@ create table bronze.crm_sales_details(
 	sls_ord_num varchar(50),
 	sls_prd_key varchar(50),
 	sls_cust_id int,
-	sls_order_dt date,
-	sls_ship_dt date,
-	sls_due_dt date,
+	sls_order_dt int,
+	sls_ship_dt int,
+	sls_due_dt int,
 	sls_sales int,
 	sls_quantity int,
 	sls_price int
